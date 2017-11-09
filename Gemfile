97b061ba853181a3a78ba1bfeb377e0de9f68aab
RRR.tg <davidruizy1983@yandex.com>
2017-11-09 11:43:14 +0800

    Add a gem file 'rack-cors'

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'bcrypt-ruby', '3.1.1.rc1', :require => 'bcrypt'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'rack-cors'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/application.rb
+++ config/application.rb
@@ -23,10 +23,10 @@ module ElmhurstBackend
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.insert_before 0, Rack::Cors do
+    config.middleware.use Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
       end
     end
   end

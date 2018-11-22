ef05b8f3882ea302bea4f3a3f178a33963046e2d
amit.dhoundiyal <ro-amit@mobiloitte.com>
2018-11-22 12:29:56 +0530

    Html Creation for the Deposit Form

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,24 +1,12 @@
 class SharesController < ApplicationController
 	def buy
-		@share_exist = Share.find_by_user_id(current_user.id)
-		if @share_exist.present?
-		 @share_exist.shares += params[:shares].to_f
-		 @share_exist.investment_principal += params[:investment_principal].to_f
-		 @share_exist.investment_principal_dup += params[:investment_principal].to_f
-		 @share_exist.is_trade_open = true
-		 # find_items_price(@share_exist.investment_principal_dup , @share_exist)
-		 @share_exist.save!
-		 flash[:success] = "Shares Buy Successfully"
-		 redirect_to "/Investor"
-		else
 		  @share = Share.new(share_params)
 		  @share.investment_principal_dup = params[:investment_principal] 
 		  @share.user_id = current_user.id
-		  @share_exist.is_trade_open = true
+		  @share.is_trade_open = true
 		  @share.save!
 		  flash[:success] = "Shares Buy Successfully"
 		  redirect_to "/Investor"
-		end
 	end
 
 	def sell
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def item_price_avg
 	  stocks = StockQuote::Stock.quote("aapl").as_json
-	  @avg = (Annotation.where.not(item_price_dup: 0).pluck(:Item_Price).compact.sum / Annotation.where.not(item_price_dup: 0).pluck(:Item_Price).compact.count) rescue nil
+      @avg = (Annotation.where.not(item_price_dup: 0).pluck(:Item_Price).compact.sum / Annotation.where.not(item_price_dup: 0).pluck(:Item_Price).compact.count) rescue 1
 	  @avg > stocks["latest_price"] ? @avg/stocks["latest_price"] : stocks["latest_price"]/@avg  rescue nil
 	end
 
@@ -11,7 +11,7 @@ module ApplicationHelper
 	end
     
     def purchasing_power
-      @purchasing_power = Share.all.pluck(:investment_principal).compact.sum rescue nil
+      @purchasing_power = (Share.all.pluck(:investment_principal).compact.sum/Share.all.count.to_f).round(4) rescue nil
       @purchasing_power ? @purchasing_power : 0 
     end
 	
@@ -28,7 +28,7 @@ module ApplicationHelper
 	#                       Share.find_by_user_id(current_user.id).shares) + @deposit_balance.to_f).round(4)  rescue nil
 	#   @purchasing_power ? @purchasing_power : 0
 	# end
-   
+
     # def fulfilled_req_avg
     #   @fullfill_req_avg = Share.where("created_at BETWEEN ? and ?",DateTime.now,DateTime.now+1).pluck(:investment_principal).compact.sum - (Annotation.all.pluck(:Item_Price).compact.sum / Annotation.all.pluck(:Item_Price).compact.count) rescue nil
     # end

c01920e44f321fe4f021efd82fe8bc9c0cef1a7e
amit.dhoundiyal <ro-amit@mobiloitte.com>
2018-12-03 19:24:16 +0530

    change in the form of new pr

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -16,8 +16,8 @@ var project_id = getCook('project_id');
 //main api url
 //var mainUrl = '//elmhur.st/api/v1/';
 //alert(user_id);
-//var mainUrl = 'https://www.elmhur.st/api/v1/';
-var mainUrl = 'http://localhost:3000/api/v1/';
+var mainUrl = 'https://www.elmhur.st/api/v1/';
+//var mainUrl = 'http://localhost:3000/api/v1/';
 
 //var requset ='https://localhost:8080/elmuhrst/rest/projectResearcher/2/1'
 

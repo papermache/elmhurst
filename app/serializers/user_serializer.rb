681a18203c1254b530c92a8619bd8c3f5dba0efc
amit.dhoundiyal <ro-amit@mobiloitte.com>
2018-12-07 18:11:29 +0530

    Remove university and researcher from the user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :encrypted_password , :address, :researcher ,:university
+  attributes :id, :email, :first_name, :last_name, :encrypted_password , :address
  
 
 end

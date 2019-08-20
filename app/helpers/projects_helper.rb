5df0d19b3f2930bd6f9068479f3acdb683731453
Kashif Ghaffar Asim <kashif.ghaffar100@gmail.com>
2019-08-20 11:07:15 +0500

    Investor and Updates

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -42,15 +42,16 @@ module ProjectsHelper
     end
   end
 
-  def profit_loss invesment_principal
-    begin
-      stocks = StockQuote::Stock.quote("aapl").as_json
+  def profit_loss(invesment_principal, client)
+#    begin
+      stocks = client.quote("aapl")
+      puts"ssssssssssssssssssssssssssssssssssssssssssssss checking profit lost", stocks.inspect
       fa = Annotation.where(item_price_dup: 0).pluck(:item_price).compact.sum rescue 1
-      @profit_loss = (((stocks["latest_price"]/fa)/invesment_principal)*100).round(4)
+      @profit_loss = (((stocks.latest_price/fa)/invesment_principal)*100).round(4)
       @profit_loss.finite? ?  @profit_loss : 0
-    rescue
-      @profit_loss = 0
-    end
+#    rescue
+#      @profit_loss = 0
+#    end
   end
 
   def find_project_creator

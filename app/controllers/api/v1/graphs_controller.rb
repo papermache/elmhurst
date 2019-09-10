6d9337de88fb47bb9fa0f9190864be54bfd86188
Sarah Strong <sarah.e.strong@gmail.com>
2019-09-10 16:06:11 -0400

    Display main graph as time series aware candlesticks

--- app/controllers/api/v1/graphs_controller.rb
+++ app/controllers/api/v1/graphs_controller.rb
@@ -7,18 +7,19 @@ module Api
 
         # Group by nearest 15 minutes for display as candlestick
         grouped = Graph.all.group_by do |x|
-          rounded_time = round_to_minutes(x.created_at, 15)
+          group_by_minutes = params[:group_by_minutes].to_i || 15
+          rounded_time = round_to_minutes(x.created_at, group_by_minutes)
           rounded_time.strftime(minutes_strf)
         end
 
         res = grouped.map do |time, graphs|
           {
             x: DateTime.strptime(time, minutes_strf).to_i,
-            open: open(graphs),
-            high: high(graphs),
-            low: low(graphs),
-            close: close(graphs),
-            items: graphs
+            open: open(graphs).round(2),
+            high: high(graphs).round(2),
+            low: low(graphs).round(2),
+            close: close(graphs).round(2),
+            item_tooltips: item_tooltips(graphs, 7)
           }
         end
 
@@ -67,6 +68,24 @@ private
       def rolling_average_price_for_index(graphs, i)
         graphs_seen = graphs[0..i + 1]
         graphs_seen.map { |g| g[:item_price] }.sum / graphs_seen.length
+      end
+
+      def item_tooltips(graphs, max_tooltips)
+        graphs = graphs.sort_by(&:created_at)
+        graphs = graphs.length <= max_tooltips ? graphs : graphs.take(max_tooltips - 1)
+        tooltips = graphs.map do |g|
+          {
+            body: [g[:item_name], g[:item_price], g[:vendor], g[:user], g[:invoice]].join("\n"),
+            url_text: 'Flag',
+            url: 'https://www.google.com'
+          }
+        end
+        if graphs.length > max_tooltips
+          tooltips << {
+            body: "#{graphs.length - tooltips.length} items not shown"
+          }
+        end
+        tooltips
       end
     end
   end
